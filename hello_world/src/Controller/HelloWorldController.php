272194ab43700f51b74a10ef0d42b83df7a37063
Pranit Jha <pranit.jha@acquia.com>
2019-06-14 16:57:28 +0530

    master: Dependency injection added for Database connection.

--- hello_world/src/Controller/HelloWorldController.php
+++ hello_world/src/Controller/HelloWorldController.php
@@ -2,15 +2,36 @@
 
 namespace Drupal\hello_world\Controller;
 
-class HelloWorldController {
-  public function hello() {
-    $result = \Drupal::database()->query('SELECT count(uid) as total_user from users WHERE uid > :uid', [':uid' => 0]);
-    $total_user = $result->fetchField();
-    \Drupal::messenger()->addMessage(t('This is a Drupal site with @total_user users.', ['@total_user' => $total_user]), 'status');
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Database\Connection;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
-    return array(
+
+class HelloWorldController extends ControllerBase {
+
+  /**
+   * @var \Drupal\Core\Database\Connection
+   */
+  protected $connection;
+
+  public function __construct(Connection $connection) {
+    $this->connection = $connection;
+  }
+
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('database')
+    );
+  }
+
+  public function hello() {
+    $result = $this->connection->query('SELECT count(uid) as total_user from users WHERE uid > :uid', [':uid' => 0]);
+    $total_user = $result->fetchField();
+    $this->messenger()->addStatus($this->t('This is a Drupal site with @total_user users.', ['@total_user' => $total_user]));
+
+    return [
       '#title' => 'Hello World!',
       '#markup' => 'Here is some content.',
-    );
+    ];
   }
 }
