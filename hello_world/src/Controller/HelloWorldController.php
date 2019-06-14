d14d0a92710aac442ee13c9dff246d10e0c46caf
Pranit Jha <pranit.jha@acquia.com>
2019-06-14 15:40:13 +0530

    master: Deprecated code is updated.

--- hello_world/src/Controller/HelloWorldController.php
+++ hello_world/src/Controller/HelloWorldController.php
@@ -4,9 +4,9 @@ namespace Drupal\hello_world\Controller;
 
 class HelloWorldController {
   public function hello() {
-    $result = db_query('SELECT count(uid) as total_user from users WHERE uid > :uid', [':uid' => 0]);
+    $result = \Drupal::database()->query('SELECT count(uid) as total_user from users WHERE uid > :uid', [':uid' => 0]);
     $total_user = $result->fetchField();
-    drupal_set_message(t('This is a Drupal site with @total_user users.', ['@total_user' => $total_user]), 'status');
+    \Drupal::messenger()->addMessage(t('This is a Drupal site with @total_user users.', ['@total_user' => $total_user]), 'status');
 
     return array(
       '#title' => 'Hello World!',
