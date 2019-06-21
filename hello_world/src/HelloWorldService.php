98aba57d20c1a91c5b817d495009defebebea42b
Pranit Jha <pranit.jha@acquia.com>
2019-06-21 16:00:10 +0530

    Injecting database service.

--- hello_world/src/HelloWorldService.php
+++ hello_world/src/HelloWorldService.php
@@ -1,10 +1,38 @@
 <?php
+
 namespace Drupal\hello_world;
 
+use Drupal\Core\Database\Connection;
+
 class HelloWorldService {
+
+  /**
+   * Database connection.
+   *
+   * @var \Drupal\Core\Database\Connection.
+   */
+  protected $connection;
+
+  /**
+   * HelloWorldService constructor.
+   *
+   * @param \Drupal\Core\Database\Connection $connection
+   *   Database connection.
+   */
+  public function __construct(Connection $connection) {
+    $this->connection = $connection;
+  }
+
+  /**
+   * Get total user count.
+   *
+   * @return mixed
+   *   Total user count.
+   */
   public function getUserCount() {
-    $result = db_query('SELECT count(uid) as total_user from users WHERE uid > :uid', [':uid' => 0]);
+    $result = $this->connection->query('SELECT count(uid) as total_user from users WHERE uid > :uid', [':uid' => 0]);
     $total_user = $result->fetchField();
     return $total_user;
   }
+
 }
